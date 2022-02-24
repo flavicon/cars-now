2fee578fd5620d262a1484b52eeb7b45d09388d2
Flavio Silva <flavio.devjs@gmail.com>
2022-02-24 06:39:52 -0300

    refactoring element

--- src/components/VisualizationCar/index.jsx
+++ src/components/VisualizationCar/index.jsx
@@ -1,9 +1,19 @@
 import { Modal } from "antd";
+import useVisualizationCar from "../../hooks/useVisualizationCar";
 
 const VisualizationCar = (props) => {
+  const [cancelVisualizationCar] = useVisualizationCar();
   console.log(props);
+  console.log(cancelVisualizationCar);
+
   return (
-    <Modal title="Oferta especial" centered width={1000}>
+    <Modal
+      visible={props.visible}
+      onCancel={() => cancelVisualizationCar()}
+      title="Oferta especial"
+      centered
+      width={1000}
+    >
       <p>Teste</p>
     </Modal>
   );
