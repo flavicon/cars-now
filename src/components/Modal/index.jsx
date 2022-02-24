a04c8cc7f0621a7af899c31174c65d68a68a8696
Flavio Silva <flavio.devjs@gmail.com>
2022-02-24 20:52:34 -0300

    alter size input

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,4 +1,4 @@
-import { Modal, Form, Input, InputNumber, DatePicker } from "antd";
+import { Space, Modal, Form, Input, InputNumber, DatePicker } from "antd";
 import moment from "moment";
 import { useDiscounts } from "../../hooks/useDiscount";
 
@@ -28,116 +28,129 @@ const ModalDiscount = (props) => {
       destroyOnClose
     >
       <Form form={formDiscount} layout="vertical" preserve={false}>
-        <Form.Item
-          name="marca"
-          label="Marca"
-          rules={[
-            {
-              required: true,
-              message: "Por favor insira a marca do veículo.",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name="modelo"
-          label="Modelo"
-          rules={[
-            {
-              required: true,
-              message: "Por favor insira o modelo do veículo.",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name="ano"
-          label="Ano"
-          rules={[
-            {
-              required: true,
-              message: "Por favor insira o ano do veículo.",
-            },
-          ]}
-        >
-          <DatePicker picker="year" />
-        </Form.Item>
-        <Form.Item
-          name="preco"
-          label="Preço"
-          rules={[
-            {
-              required: true,
-              message: "Por favor insira o preço do veículo.",
-            },
-          ]}
-        >
-          <InputNumber addonBefore="R$" />
-        </Form.Item>
-        <Form.Item
-          name="cor"
-          label="Cor"
-          rules={[
-            {
-              required: true,
-              message: "Por favor insira a cor do veículo.",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item name="km" label="Quilometragem">
-          <InputNumber addonAfter="KM" min={0} />
-        </Form.Item>
-        <Form.Item
-          name="placa"
-          label="Placa"
-          rules={[
-            {
-              required: true,
-              message: "Por favor insira a placa do veículo.",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name="cidade"
-          label="Cidade"
-          rules={[
-            {
-              required: true,
-              message: "Por favor insira a cidade.",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
+        <Space size="middle">
+          <Form.Item
+            name="marca"
+            label="Marca"
+            rules={[
+              {
+                required: true,
+                message: "Por favor insira a marca do veículo.",
+              },
+            ]}
+          >
+            <Input size="large" />
+          </Form.Item>
+          <Form.Item
+            name="modelo"
+            label="Modelo"
+            rules={[
+              {
+                required: true,
+                message: "Por favor insira o modelo do veículo.",
+              },
+            ]}
+          >
+            <Input size="large" />
+          </Form.Item>
+        </Space>
+
+        <Space size="middle">
+          <Form.Item
+            name="cor"
+            label="Cor"
+            rules={[
+              {
+                required: true,
+                message: "Por favor insira a cor do veículo.",
+              },
+            ]}
+          >
+            <Input size="large" />
+          </Form.Item>
+          <Form.Item name="km" label="Quilometragem">
+            <InputNumber size="large" addonAfter="KM" min={0} />
+          </Form.Item>
+        </Space>
+        <Space size="middle">
+          <Form.Item
+            name="placa"
+            label="Placa"
+            rules={[
+              {
+                required: true,
+                message: "Por favor insira a placa do veículo.",
+              },
+            ]}
+          >
+            <Input size="large" />
+          </Form.Item>
+          <Form.Item
+            name="cidade"
+            label="Cidade"
+            rules={[
+              {
+                required: true,
+                message: "Por favor insira a cidade.",
+              },
+            ]}
+          >
+            <Input size="large" />
+          </Form.Item>
+        </Space>
+        <Space size="middle">
+          <Form.Item
+            name="preco"
+            label="Preço"
+            rules={[
+              {
+                required: true,
+                message: "Por favor insira o preço do veículo.",
+              },
+            ]}
+          >
+            <InputNumber size="large" addonBefore="R$" />
+          </Form.Item>
+          <Form.Item
+            name="ano"
+            label="Ano"
+            rules={[
+              {
+                required: true,
+                message: "Por favor insira o ano do veículo.",
+              },
+            ]}
+          >
+            <DatePicker size="large" picker="year" />
+          </Form.Item>
+        </Space>
         <Form.Item
           name="foto"
           label="Foto"
+          tooltip="Insira um link formato url."
           rules={[
             {
+              type: "url",
               required: true,
-              message: "Por favor insira a foto do veículo.",
+              message: "Por favor insira um link url válido.",
             },
           ]}
         >
-          <Input />
+          <Input size="large" />
         </Form.Item>
         <Form.Item
           name="fotoAdicional"
           label="Foto adicional"
+          tooltip="Insira um link formato url."
           rules={[
             {
+              type: "url",
               required: true,
-              message: "Por favor insira a foto adicional do veículo.",
+              message: "Por favor insira um link url válido.",
             },
           ]}
         >
-          <Input />
+          <Input size="large" />
         </Form.Item>
       </Form>
     </Modal>
