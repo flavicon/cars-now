ead2db36506e7402b9c49ec34bd543889f04279f
Flavio Silva <flavio.devjs@gmail.com>
2022-02-23 23:15:16 -0300

    create layout list to page outlet

---
+++ src/components/CardList/index.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Space, Tag, List, Divider, Typography } from "antd";
+import { EyeOutlined } from "@ant-design/icons";
+
+const { Title } = Typography;
+
+const IconText = ({ icon, text }) => (
+  <Space>
+    {React.createElement(icon)}
+    {text}
+  </Space>
+);
+
+const CardList = (props) => {
+  return (
+    <List
+      itemLayout="vertical"
+      size="large"
+      style={{ maxWidth: "1000px" }}
+      dataSource={props.dataSource}
+      renderItem={(item) => (
+        <List.Item
+          onClick={() => console.log(item)}
+          key={item.key}
+          extra={
+            <img
+              width={250}
+              style={{ borderRadius: "15px" }}
+              alt="car"
+              src={item.foto}
+            />
+          }
+        >
+          <List.Item.Meta title={item.modelo} description="Oferta especial" />
+          Confira está grande oportunidade que temos para você. <br /> Veiculos
+          altamente confortáveis, com quatro rodas, cinco assentos e o valor
+          naquele preço.
+          <Divider />
+          <Tag color="blue">Ano: {item.ano}</Tag>
+          <Tag color="cyan">Marca: {item.marca}</Tag>
+          <Divider />
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <Title level={4}>R$ {item.preco}</Title>
+            <IconText icon={EyeOutlined} text="100 visalizações" />
+          </div>
+        </List.Item>
+      )}
+    />
+  );
+};
+
+export default CardList;
