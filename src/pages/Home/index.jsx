66befad98078de70f974b572320e882cb4291400
Flavio Silva <flavio.devjs@gmail.com>
2022-02-23 23:12:16 -0300

    refactoring page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,32 +1,56 @@
 import { useNavigate, Outlet } from "react-router-dom";
-import { Menu } from "antd";
+import { Menu, Layout } from "antd";
 import { DollarOutlined, UserOutlined } from "@ant-design/icons";
 
-const menuStyles = { display: "flex", justifyContent: "center" };
+const { Header, Footer, Content } = Layout;
+
+const menuStyles = { display: "flex", justifyContent: "end" };
 
 const Home = () => {
   let navigate = useNavigate();
 
   return (
-    <>
-      <Menu mode="horizontal" style={menuStyles}>
-        <Menu.Item
-          key="outlet"
-          icon={<DollarOutlined />}
-          onClick={() => navigate("/ofertas")}
+    <Layout>
+      <Header
+        style={{
+          background: "white",
+          position: "fixed",
+          zIndex: 1,
+          width: "100%",
+        }}
+      >
+        <Menu mode="horizontal" style={menuStyles}>
+          <Menu.Item
+            key="outlet"
+            icon={<DollarOutlined />}
+            onClick={() => navigate("/ofertas")}
+          >
+            Ofertas
+          </Menu.Item>
+          <Menu.Item
+            key="management"
+            icon={<UserOutlined />}
+            onClick={() => navigate("/administracao")}
+          >
+            Administração
+          </Menu.Item>
+        </Menu>
+      </Header>
+      <Layout>
+        <Content
+          style={{
+            minHeight: "100vh",
+            backgroundColor: "white",
+            marginTop: 64,
+          }}
         >
-          Ofertas
-        </Menu.Item>
-        <Menu.Item
-          key="management"
-          icon={<UserOutlined />}
-          onClick={() => navigate("/administracao")}
-        >
-          Administração
-        </Menu.Item>
-      </Menu>
-      <Outlet />
-    </>
+          <Outlet />
+        </Content>
+      </Layout>
+      <Footer style={{ textAlign: "center" }}>
+        Powered by flavicon systems &copy;
+      </Footer>
+    </Layout>
   );
 };
 
