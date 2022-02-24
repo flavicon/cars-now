f53ff4512f967a37fee960a3dbadeebcb1f9ab1a
Flavio Silva <flavio.devjs@gmail.com>
2022-02-23 23:16:02 -0300

    create modal to visualization of cars informations

---
+++ src/components/VisualizationCar/index.jsx
@@ -0,0 +1,12 @@
+import { Modal } from "antd";
+
+const VisualizationCar = (props) => {
+  console.log(props);
+  return (
+    <Modal title="Oferta especial" centered width={1000}>
+      <p>Teste</p>
+    </Modal>
+  );
+};
+
+export default VisualizationCar;
