c050976d54f092ac397abb6af2d3a098f0d4c753
Flavio Silva <flavio.devjs@gmail.com>
2022-02-24 20:55:54 -0300

    create function to update views

--- src/hooks/useVisualizationCar.jsx
+++ src/hooks/useVisualizationCar.jsx
@@ -1,18 +1,45 @@
 import { useState } from "react";
 import VisualizationCar from "../components/VisualizationCar";
+import { doc, updateDoc } from "firebase/firestore/lite";
+import { db } from "../services/firebase";
+import { useDiscounts } from "../hooks/useDiscount";
 
 const useVisualizationCar = () => {
   const [visibleVisualizationCar, setVisibleVisualizationCar] = useState(false);
   const [discountItem, setDiscountItem] = useState({});
+  const { discounts, setDiscounts } = useDiscounts();
 
   const showVisualizationCar = (item) => {
     setDiscountItem(item);
     setVisibleVisualizationCar(true);
+
+    updateViewsDiscount(item);
   };
 
   const cancelVisualizationCar = () => {
     setVisibleVisualizationCar(false);
     setDiscountItem({});
+  };
+
+  const updateViewsDiscount = async (item) => {
+    const updateDiscount = [...discounts];
+    const discountExists = updateDiscount.find(
+      (discount) => discount.key === item.key
+    );
+
+    try {
+      if (discountExists) {
+        // update state discounts
+        discountExists.visualizacoes += 1;
+        setDiscounts(updateDiscount);
+
+        // update on firebase
+        const discountRef = doc(db, "ofertas", item.key);
+        await updateDoc(discountRef, {
+          visualizacoes: discountExists.visualizacoes,
+        });
+      }
+    } catch {}
   };
 
   return [
