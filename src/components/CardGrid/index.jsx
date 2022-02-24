0f1d0cdac0a6354c75c89d520b6b7de56f78d654
Flavio Silva <flavio.devjs@gmail.com>
2022-02-23 23:14:40 -0300

    Create layout grid to page outlet

---
+++ src/components/CardGrid/index.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Space, Card, List, Divider, Typography, Tag } from "antd";
+import { EyeOutlined } from "@ant-design/icons";
+
+const { Meta } = Card;
+const { Title } = Typography;
+
+const IconText = ({ icon, text }) => (
+  <Space>
+    {React.createElement(icon)}
+    {text}
+  </Space>
+);
+
+const CardGrid = (props) => {
+  return (
+    <List
+      itemLayout="horizontal"
+      grid={{ gutter: 16, column: 3 }}
+      size="large"
+      style={{ maxWidth: "1000px" }}
+      dataSource={props.dataSource}
+      renderItem={(item) => (
+        <List.Item>
+          <Card
+            hoverable
+            style={{ width: 300 }}
+            title={item.modelo}
+            cover={<img alt="car" src={item.foto} />}
+          >
+            <Meta
+              title="Oferta especial"
+              description="Confira está grande oportunidade que temos para você. 
+              Veiculos altamente confortáveis, com quatro rodas, cinco assentos
+                e o valor naquele preço."
+            />
+            <Divider />
+            <Tag color="blue">Ano: {item.ano}</Tag>
+            <Tag color="cyan">Marca: {item.marca}</Tag>
+            <Divider />
+            <Title level={4}>R$ {item.preco}</Title>
+            <IconText icon={EyeOutlined} text="100 visalizações" />
+          </Card>
+        </List.Item>
+      )}
+    />
+  );
+};
+
+export default CardGrid;
