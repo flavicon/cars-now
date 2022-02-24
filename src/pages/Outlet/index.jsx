d91c448219f32d3171d30d96f8bf3e7fb9ad424f
Flavio Silva <flavio.devjs@gmail.com>
2022-02-24 06:41:18 -0300

    Modified page and added component visualizationCar

--- src/pages/Outlet/index.jsx
+++ src/pages/Outlet/index.jsx
@@ -4,49 +4,53 @@ import { useDiscounts } from "../../hooks/useDiscount";
 import { useState } from "react";
 import CardList from "../../components/CardList";
 import CardGrid from "../../components/CardGrid";
+import useVisualizationCar from "../../hooks/useVisualizationCar";
 
 const { Title } = Typography;
 
 const Outlet = () => {
   const [layout, setLayout] = useState("list");
+  const [visualizationCar, showVisualizationCar] = useVisualizationCar();
   const { discounts } = useDiscounts();
 
   const menuList = () => setLayout("list");
-
   const menuGrid = () => setLayout("grid");
 
   return (
-    <section
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        marginTop: 16,
-      }}
-    >
-      <Title level={2}>Ofertas especiais</Title>
-      <div
+    <>
+      <section
         style={{
-          width: "100%",
-          maxWidth: "1000px",
-          padding: "24px",
           display: "flex",
-          justifyContent: "start",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: 16,
         }}
       >
-        <Button onClick={menuList} size="middle" style={{ marginRight: 8 }}>
-          <BarsOutlined />
-        </Button>
-        <Button onClick={menuGrid}>
-          <AppstoreOutlined />
-        </Button>
-      </div>
-      {layout === "list" ? (
-        <CardList dataSource={discounts} />
-      ) : (
-        <CardGrid dataSource={discounts} />
-      )}
-    </section>
+        <Title level={2}>Ofertas especiais</Title>
+        <div
+          style={{
+            width: "100%",
+            maxWidth: "1000px",
+            padding: "24px",
+            display: "flex",
+            justifyContent: "start",
+          }}
+        >
+          <Button onClick={menuList} size="middle" style={{ marginRight: 8 }}>
+            <BarsOutlined />
+          </Button>
+          <Button onClick={menuGrid}>
+            <AppstoreOutlined />
+          </Button>
+        </div>
+        {layout === "list" ? (
+          <CardList onClick={showVisualizationCar} dataSource={discounts} />
+        ) : (
+          <CardGrid dataSource={discounts} />
+        )}
+      </section>
+      {visualizationCar}
+    </>
   );
 };
 
