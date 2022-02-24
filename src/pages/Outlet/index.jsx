c26e4ead752589e0d56591f22284104dda46c1ef
Flavio Silva <flavio.devjs@gmail.com>
2022-02-23 23:13:59 -0300

    create components and layout to page

--- src/pages/Outlet/index.jsx
+++ src/pages/Outlet/index.jsx
@@ -1,5 +1,53 @@
+import { Button, Typography } from "antd";
+import { BarsOutlined, AppstoreOutlined } from "@ant-design/icons";
+import { useDiscounts } from "../../hooks/useDiscount";
+import { useState } from "react";
+import CardList from "../../components/CardList";
+import CardGrid from "../../components/CardGrid";
+
+const { Title } = Typography;
+
 const Outlet = () => {
-  return <div>Ofertas</div>;
+  const [layout, setLayout] = useState("list");
+  const { discounts } = useDiscounts();
+
+  const menuList = () => setLayout("list");
+
+  const menuGrid = () => setLayout("grid");
+
+  return (
+    <section
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        marginTop: 16,
+      }}
+    >
+      <Title level={2}>Ofertas especiais</Title>
+      <div
+        style={{
+          width: "100%",
+          maxWidth: "1000px",
+          padding: "24px",
+          display: "flex",
+          justifyContent: "start",
+        }}
+      >
+        <Button onClick={menuList} size="middle" style={{ marginRight: 8 }}>
+          <BarsOutlined />
+        </Button>
+        <Button onClick={menuGrid}>
+          <AppstoreOutlined />
+        </Button>
+      </div>
+      {layout === "list" ? (
+        <CardList dataSource={discounts} />
+      ) : (
+        <CardGrid dataSource={discounts} />
+      )}
+    </section>
+  );
 };
 
 export default Outlet;
